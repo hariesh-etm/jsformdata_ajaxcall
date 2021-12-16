ac631f09bf4b9d6988ba0970a1233f209ba8dcdb
hariesh-etm <95334384+hariesh-etm@users.noreply.github.com>
2021-12-16 19:09:22 +0530

    3

--- mail1.php
+++ mail1.php
@@ -1,29 +1,12 @@
 <?php
-//  var_dump($_POST);
-//  var_dump($_FILES);
-//  exit;
-// $json = file_get_contents('php://input');
-// $data = json_decode($json);
-    
-//        // $name = $data->name;
-//         echo "test";
-//         print_r($json);
-//         exit;
-//         $email = $data->email;
-//         $subject = $data->subject;
-//         $message = $data->message;
-        
-       
 
+        error_reporting(~E_WARNING & ~E_NOTICE);
         $name = $_POST['name'];
         $email = $_POST['email'];
         $subject = $_POST['subject'];
         $message = $_POST['message'];  
         $resume=$_FILES['resume'];    
-        
-        
-      
-
+           
         $error=false;
         // $errormessage = array();
         $errmsg = "";
@@ -79,10 +62,11 @@
                             $target_dir = "resumes/";
                             $target_file = $target_dir . basename($_FILES["resume"]["name"]);
                             $uploadOk = 1;
-                            $fileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+                            //$fileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+                            
                            
                             if (move_uploaded_file($_FILES["resume"]["tmp_name"], $target_file)) {
-                                echo "The file ". htmlspecialchars( basename( $_FILES["resume"]["name"])). " has been uploaded.";
+                                // echo "The file ". htmlspecialchars( basename( $_FILES["resume"]["name"])). " has been uploaded.";
                               } else {
                                 echo "Sorry, there was an error uploading your file.";
                               }
@@ -90,8 +74,8 @@
 
 
 
-                        $sql = "INSERT INTO `contact` (`id`,`name`, `email`, `subject`,`message`)
-                        VALUES (NULL,'$name', '$email', '$subject','$message')";
+                        $sql = "INSERT INTO `contact` (`id`,`name`, `email`, `subject`,`message`,`resume`)
+                        VALUES (NULL,'$name', '$email', '$subject','$message','$target_file')";
                             }
                              if (mysqli_query($conn, $sql)) { 
                                  $data = array(
